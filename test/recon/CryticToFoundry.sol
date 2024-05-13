4f498cebb302021fdd9684ceae65b9e107bfcf15
Simon Busch <Simon.busch@live.fr>
2024-05-13 11:11:29 +0200

    recon setup

---
+++ test/recon/CryticToFoundry.sol
@@ -0,0 +1,19 @@
+
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+import {TargetFunctions} from "./TargetFunctions.sol";
+import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
+
+contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
+    function setUp() public {
+        setup();
+    }
+
+    function testDemo() public {
+      counter_setNumber(18446744073709551615);
+      counter_increment();
+    }
+}
